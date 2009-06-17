a34fa353ee5fc25989c7ec4079ab2a9ee8c35759
Joshua Gardner <mellowcellofellow@gmail.com>
2009-06-16 20:47:16 -0600

    Fixed get_edit_url for all models that implement it. Missed a few last time. :P

--- library/models.py
+++ library/models.py
@@ -30,7 +30,7 @@ class Arranger(models.Model):
 		return ('library.views.arranger_list', [str(self.id)])
 	
 	def get_edit_url(self):
-		return '/admin/library/arranger/%d/' % self.id
+		return '%s/admin/library/arranger/%d/' % (URL_PREFIX, self.id)
 	
 	class Meta:
 		ordering = ['last_name']
@@ -62,7 +62,7 @@ class Piece(models.Model):
 		return ('piece_detail', (), { 'object_id': self.id })
 	
 	def get_edit_url(self):
-		return '/admin/library/piece/%d/' % self.id
+		return '%s/admin/library/piece/%d/' % (URL_PREFIX, self.id)
 	
 	class Meta:
 		ordering = ['title']
@@ -86,7 +86,7 @@ class CabinetGroup(models.Model):
 		return ('library.views.group_list', (), {'group_name': self.shortname})
 	
 	def get_edit_url(self):
-		return '/admin/library/cabinetgroup/%d/' % self.id
+		return '%s/admin/library/cabinetgroup/%d/' % self.id
 
 class Cabinet(models.Model):
 	number = models.IntegerField('Cabinet ID Number')
@@ -106,7 +106,7 @@ class Cabinet(models.Model):
 		})
 	
 	def get_edit_url(self):
-		return '/admin/library/cabinet/%d/' % self.id
+		return '%s/admin/library/cabinet/%d/' % self.id
 	
 class Drawer(models.Model):
 	cabinet = models.ForeignKey('Cabinet')
@@ -127,7 +127,7 @@ class Drawer(models.Model):
 		})
 	
 	def get_edit_url(self):
-		return '/admin/library/drawer/%d/' % self.id
+		return '%s/admin/library/drawer/%d/' % self.id
 
 class Orchestra(models.Model):
 	shortname = models.CharField('Short Name', max_length=5, unique=True)
@@ -150,4 +150,4 @@ class Performance(models.Model):
 		return ('performance_detail', (), { 'object_id': self.id })
 	
 	def get_edit_url(self):
-		return '/admin/library/performance/%d/' % self.id
+		return '%s/admin/library/performance/%d/' % self.id
