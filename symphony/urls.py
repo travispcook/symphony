fc8e352b2180b3b9bbb703abcea386ed6c699d4d
Robert Stewart <rstewart@izeni.com>
2013-05-04 17:21:35 -0600

    Angular demos

--- symphony/urls.py
+++ symphony/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls.defaults import *
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -11,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^foo/', include('foo.foo.urls')),
     (r'^', include('library.urls')),
     (r'^api/', include('library.api_urls')),
+    (r'^angular/', include('angular_demo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
