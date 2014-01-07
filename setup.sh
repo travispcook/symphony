068d89b535cb5991ed6c5c0953c7981f941d71f6
Joshua Gardner <mellowcellofellow@gmail.com>
2014-01-06 22:20:01 -0700

    Run management commands in setup.sh.

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ echo "Initialize Database"
 mkdir -p /opt/symphony/system/database
 chown postgres:postgres /opt/symphony/system/database
 su - postgres -c "initdb -E utf-8 --locale=en_US.UTF-8 /opt/symphony/system/database"
+su - developer -c "/opt/symphony/manage.py bower_install"
+su - developer -c "/opt/symphony/manage.py collectstatic"
